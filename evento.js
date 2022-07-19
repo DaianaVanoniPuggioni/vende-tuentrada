039d228f0eb007b9a2a69933c24f895f3099841d
guido fortunato <guido_fortunato@hotmail.com.ar>
2022-07-19 12:26:51 -0300

    cambios

--- evento.js
+++ evento.js
@@ -1,13 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
+    
     var nav = document.getElementById("nav");
-    var current_pos = nav.offsetTop;
+    // var current_pos = nav.offsetTop;
+    // console.log(current_pos)
     window.onscroll = function(){
         var window_pos = document.documentElement.scrollTop;
-        if(window_pos>=current_pos){
-            nav.classList.add("sticky");
+        if(window_pos>=200){
+            nav.classList.add("color-nav");
         }
         else{
-            nav.classList.remove("sticky");
+            nav.classList.remove("color-nav");
         }
     }
 } )
+
